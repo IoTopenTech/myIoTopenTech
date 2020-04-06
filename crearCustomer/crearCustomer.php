011617db09eda952b6c0467891f2491780afdf64
IoT open Tech <contacto@iotopentech.io>
2020-04-06 20:34:54 +0200

    CrearCustomer

---
+++ crearCustomer/crearCustomer.php
@@ -0,0 +1,15 @@
+Nodo01
+curl -v -X POST -d "{\"digital_in_2\": \"1\"}" https://tb.iotopentech.io/api/v1/bOi1EVgYRgC39QUAGu1k/telemetry --header "Content-Type:application/json"
+
+Nodo02
+curl -v -X POST -d "{\"digital_in_2\": \"1\"}" https://tb.iotopentech.io/api/v1/FpeOpgvxK0C2atfpde5R/telemetry --header "Content-Type:application/json"
+
+
+Nodo01: c0ed5650-5e47-11ea-914a-d3f42a2c349b
+Nodo02: e964be00-6628-11ea-a5d1-174bec7b16a7
+Control:e06cac60-66df-11ea-a5d1-174bec7b16a7
+
+Activo ROOT:   b88f5360-6765-11ea-a5d1-174bec7b16a7
+Activo MiCasa: bb26fc70-662c-11ea-a5d1-174bec7b16a7
+
+coHcmZGq0cFwYGAQsPUN
